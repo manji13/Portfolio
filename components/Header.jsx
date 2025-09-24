1bc552674db48313cc36ea7b3e4490fdb0175c43
manjitha kavishan <manjikavi8@gmail.com>
2025-09-24 18:04:27 +0530

    Initial commit

---
+++ components/Header.jsx
@@ -0,0 +1,39 @@
+"use client";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import Nav from "./nav";
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white">
+        <div className="container mx-auto flex justify-between items-center">
+            
+
+            <Link href={"/"}>
+             <h1 className="text-3xl font-semibold">
+                Manji <span className="text-accent">.</span>
+             </h1>
+            </Link>
+
+
+            <div className="hidden xl:flex item-center gap-8">
+              <Nav />
+              <Link href="/contact">
+                <button> Hire me</button>
+              </Link>
+            </div>
+
+            <div className="xl:hidden">
+             mobile nav    
+            </div> 
+
+        </div>
+    </header>
+  )
+}
+
+export default Header;
+
+
+
+
